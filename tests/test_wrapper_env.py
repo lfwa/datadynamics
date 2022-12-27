2823c5d19c760f2a9b909618938eda788b8d4300
lfwa <lassewolffanthony@gmail.com>
2022-12-27 22:35:17 +0100

    Test multiple number of agents in wrapper env

--- tests/test_wrapper_env.py
+++ tests/test_wrapper_env.py
@@ -1,27 +1,37 @@
 from collector import collector_v0
 
 
-def test_wrapper():
-    env = collector_v0.env(
-        n_points=100,
-        n_agents=2,
-        max_collect=[100, 100],
-        render_mode="rgb_array",
-    )
-    retries = 2
-    for _ in range(retries):
-        actions = []
-        for i in range(100):
-            actions.append(i)
-            actions.append(99 - i)
-        actions.append(None)
-        actions.append(None)
-        env.reset()
-        for agent in env.agent_iter():
-            observation, reward, termination, truncation, info = env.last()
-            action = actions.pop(0)
-            env.step(action)
+def test_wrapper(retries, n_points_list, n_agents_list, max_collect_list):
+    for n_points, n_agents, max_collect in zip(
+        n_points_list, n_agents_list, max_collect_list
+    ):
+        env = collector_v0.env(
+            n_points=n_points,
+            n_agents=n_agents,
+            max_collect=max_collect,
+            render_mode="human",
+        )
+        for _ in range(retries):
+            env.reset()
+            for agent in env.agent_iter():
+                observation, reward, termination, truncation, info = env.last()
+                action = (
+                    env.action_space(agent).sample()
+                    if not termination
+                    else None
+                )
+                env.step(action)
 
 
 if __name__ == "__main__":
-    test_wrapper()
+    n_agents_list = [1, 2, 3, 5]
+    n_points_list = [100 for i in range(len(n_agents_list))]
+    max_collect_list = [
+        [10 for i in range(n_agents)] for n_agents in n_agents_list
+    ]
+    test_wrapper(
+        retries=2,
+        n_points_list=n_points_list,
+        n_agents_list=n_agents_list,
+        max_collect_list=max_collect_list,
+    )
