47745874a6e3ae45079c33e06e2816640e36c695
lfwa <lassewolffanthony@gmail.com>
2023-01-18 17:18:45 +0100

    Add example to showcase use of wrapper environment

---
+++ tutorials/example.py
@@ -0,0 +1,12 @@
+from datacollect import collector_v0
+from datacollect.policies import greedy_policy
+
+env = collector_v0.env(
+    n_points=100, n_agents=2, max_collect=[10, 90], render_mode="human"
+)
+policy = greedy_policy.GreedyPolicy(env=env)
+env.reset()
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+    action = policy.action(observation, agent)
+    env.step(action)
