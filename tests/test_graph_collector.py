909eb0d533ad0bc9bce35d518443c61083cd765f
lfwa <lassewolffanthony@gmail.com>
2023-02-20 00:20:52 +0100

    Add test for graph collector with greedy policy

---
+++ tests/test_graph_collector.py
@@ -0,0 +1,52 @@
+import networkx as nx
+import numpy as np
+import pytest
+
+from datacollect.environments import graph_collector_v0
+from datacollect.policies import greedy_policy_v0
+
+
+@pytest.mark.parametrize(
+    ("graph, point_labels, init_agent_labels, max_collect"),
+    [
+        (
+            nx.from_numpy_array(
+                np.array(
+                    [
+                        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+                        [1, 1, 1, 0, 1, 0, 0, 0, 0],
+                        [0, 1, 1, 0, 0, 1, 0, 0, 0],
+                        [1, 0, 0, 1, 1, 0, 1, 0, 0],
+                        [0, 1, 0, 1, 1, 1, 0, 0, 0],
+                        [0, 0, 1, 0, 1, 1, 0, 0, 1],
+                        [0, 0, 0, 1, 0, 0, 1, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 1, 0, 0, 1],
+                    ]
+                )
+            ),
+            [2, 3, 8],
+            [0, 1],
+            [3, 3],
+        )
+    ],
+)
+def test_graph_collector(
+    graph,
+    point_labels,
+    init_agent_labels,
+    max_collect,
+):
+    env = graph_collector_v0.env(
+        graph=graph,
+        point_labels=point_labels,
+        init_agent_labels=init_agent_labels,
+        max_collect=max_collect,
+        render_mode="rgb_array",
+    )
+    policy = greedy_policy_v0.policy(env=env)
+    env.reset()
+    for agent in env.agent_iter():
+        observation, reward, termination, truncation, info = env.last()
+        action = policy.action(observation, agent)
+        env.step(action)
