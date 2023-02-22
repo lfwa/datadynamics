f8da8160bec4ea73b347ce7de70ad684504df212
lfwa <lassewolffanthony@gmail.com>
2023-02-22 18:19:08 +0100

    Add script for generating graphs from obstacle masks

---
+++ datacollect/utils/graph_utils/grapher.py
@@ -0,0 +1,113 @@
+import networkx as nx
+import numpy as np
+import tqdm
+from PIL import Image
+
+
+def _represents_obstacle(pixel, inverted=False):
+    # Only intended for binary images.
+    return not (pixel ^ inverted)
+
+
+def _get_neighbors(i, j, m, n):
+    neighbors = []
+    if i > 0:
+        neighbors.append((i - 1, j))
+    if i < m - 1:
+        neighbors.append((i + 1, j))
+    if j > 0:
+        neighbors.append((i, j - 1))
+    if j < n - 1:
+        neighbors.append((i, j + 1))
+    return neighbors
+
+
+def from_mask_file(filename, resize=None, default_weight=1.0, inverted=False):
+    """Generate graph from an image file representing obstacle mask.
+
+    Args:
+        filename (str): Path to image file representing obstaacle mask to
+            generate graph from.
+        resize (tuple, optional): Resize image to specified width and height.
+            Defaults to None (no resizing).
+        default_weight (float, optional): Default weight of added edges.
+            Defaults to 1.0.
+        inverted (bool, optional): Invert obstacle representations. If set to
+            False then 0/False represents obstacles, i.e., nodes with no
+            incoming or outgoing edges. Similarly, if set to True then 1/True
+            represents obstacles.
+
+    Returns:
+        tuple(nx.Graph, dict): Tuple of weighted undirected graph representing
+            the image array and a dict containing metadata about the graph.
+    """
+    image = Image.open(filename)
+    if resize:
+        image = image.resize(resize)
+    # Convert to binary image.
+    image = image.convert("1")
+    image_array = np.array(image)
+    graph, metadata = from_image_array(
+        image_array, default_weight=default_weight, inverted=inverted
+    )
+    metadata["obstacle_mask_file"] = filename
+    metadata["resize"] = resize
+    return graph, metadata
+
+
+def from_image_array(image_array, default_weight=1.0, inverted=False):
+    """Generate graph from a binary image array.
+
+    Args:
+        image_array (np.ndarray): Binary image array to generate graph from.
+        default_weight (float, optional): Default weight of added edges.
+            Defaults to 1.0.
+        inverted (bool, optional): Invert obstacle representations. If set to
+            False then 0/False represents obstacles, i.e., nodes with no
+            incoming or outgoing edges. Similarly, if set to True then 1/True
+            represents obstacles.
+
+    Returns:
+        tuple(nx.Graph, dict): Tuple of weighted undirected graph representing
+            the image array and a dict containing metadata about the graph.
+    """
+    graph = nx.Graph()
+    graph.add_nodes_from(range(image_array.size))
+    m, n = image_array.shape
+
+    # Add weighted edges to graph between adjacent grid cells in image array.
+    with tqdm.tqdm(total=m * n, desc="Adding edges to graph") as pbar:
+        for i in range(m):
+            for j in range(n):
+                obstacle = _represents_obstacle(image_array[i, j], inverted)
+                node_label = i * n + j
+                neighbors = _get_neighbors(i, j, m, n)
+
+                # Add self loop for non-obstacles.
+                if not obstacle:
+                    graph.add_edge(
+                        node_label, node_label, weight=default_weight
+                    )
+
+                    # Add edges to neighbors for non-obstacles.
+                    for neighbor in neighbors:
+                        n_i, n_j = neighbor
+                        n_obstacle = _represents_obstacle(
+                            image_array[n_i, n_j], inverted
+                        )
+
+                        if not n_obstacle:
+                            graph.add_edge(
+                                node_label,
+                                n_i * n + n_j,
+                                weight=default_weight,
+                            )
+                pbar.update(1)
+    metadata = {
+        "default_weight": default_weight,
+        "inverted": inverted,
+        "nodes_per_row": image_array.shape[1],
+        "nodes_per_col": image_array.shape[0],
+        "nodes": len(graph.nodes),
+    }
+    return graph, metadata
---
+++ scripts/graph/generate_graph.py
@@ -0,0 +1,84 @@
+"""Script to generate input graphs to the graph_collector_v0 environment.
+
+This script generates a graph from an obstacle mask file with a grid-like
+structure. The input obstacle mask file should be a binary image or an image
+that can be converted to one. The file should be a mask that represents the
+obstacles in the grid-like environment that the agent cannot traverse through.
+
+Example usage:
+python -m datacollect.scripts.graph.generate_graph \
+    -omf ./data/obstacle_mask.png \
+    -o ./data/graph.pkl \
+    -m ./data/metadata.json \
+    -rs 100 100 \
+    -dfw 1.0
+"""
+import argparse
+import json
+import pickle
+
+from datacollect.utils.graph_utils import grapher
+
+
+def main(args):
+    graph, metadata = grapher.from_mask_file(
+        args.obstacle_mask_file,
+        resize=args.resize,
+        default_weight=args.default_weight,
+        inverted=args.inverted,
+    )
+
+    with open(args.output_file, "wb") as f:
+        pickle.dump(graph, f)
+        print(f"Saved graph to {args.output_file}.")
+    with open(args.metadata, "w") as f:
+        json.dump(metadata, f, indent=4)
+        print(f"Saved metadata to {args.metadata}.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-omf",
+        "--obstacle_mask_file",
+        type=str,
+        help="Obstacle mask file to generate graph from",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        type=str,
+        help="Output file to save graph to",
+        default="graph.pkl",
+    )
+    parser.add_argument(
+        "-m",
+        "--metadata",
+        type=str,
+        help="Metadata file to save graph to",
+        default="metadata.json",
+    )
+    parser.add_argument(
+        "-inv",
+        "--inverted",
+        action="store_true",
+        help="Enable inverted obstacle mask where black is obstacle and white is free space",
+    )
+    parser.add_argument(
+        "-rs",
+        "--resize",
+        type=int,
+        nargs=2,
+        help="Resize image to specified width and height",
+    )
+    parser.add_argument(
+        "-dfw",
+        "--default_weight",
+        type=float,
+        help="Default weight of added edges",
+        default=1.0,
+    )
+    args = parser.parse_args()
+
+    main(args)
