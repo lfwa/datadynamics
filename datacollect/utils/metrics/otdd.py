0c56aa65f88527f7d611b676c3176454b724fb2f
lfwa <lassewolffanthony@gmail.com>
2023-03-04 13:47:07 +0100

    Add OTDD metric for comparing simulation results

---
+++ datacollect/utils/metrics/otdd.py
@@ -0,0 +1,62 @@
+import pickle
+
+from otdd.pytorch.datasets import dataset_from_numpy
+from otdd.pytorch.distance import DatasetDistance
+
+from datacollect.utils.post_processing import extract
+
+
+def otdd(
+    d1_collections_filename, d2_collections_filename, include_timestamps=True
+):
+    """Optimal transport dataset distance between two collections over time.
+
+    The collections must be of equal length and created using
+    datacollect.utils.post_processing.save_collections. We use Microsoft's
+    OTDD library to compute the distance between the collections for each
+    timestamp to see how the distance changes over time during the simulation.
+
+    Note:
+        We skip any timestamps for which the distance cannot be computed.
+
+    Args:
+        d1_collections_filename (str): The filename of the first collection.
+        d2_collections_filename (str): The filename of the second collection.
+        include_timestamps (bool, optional): Whether to include timestamps in
+            the input features. Defaults to True.
+
+    Returns:
+        tuple: A tuple of two lists. The first list contains the timestamps
+            for which the distance was computed. The second list contains the
+            distances for each timestamp.
+    """
+    with open(d1_collections_filename, "rb") as f:
+        d1_collections = pickle.load(f)
+    with open(d2_collections_filename, "rb") as f:
+        d2_collections = pickle.load(f)
+
+    d1_timestamps, d1_feats, d1_targets = extract.feats_targets_timestamps(
+        d1_collections, include_timestamps
+    )
+    d2_timestamps, d2_feats, d2_targets = extract.feats_targets_timestamps(
+        d2_collections, include_timestamps
+    )
+    n1, n2 = len(d1_timestamps), len(d2_timestamps)
+    assert n1 == n2, "The collections must be of equal length."
+
+    completed_timestamps = []
+    distances = []
+
+    for i in range(1, n1 + 1):
+        try:
+            d1 = dataset_from_numpy(d1_feats[:i], d1_targets[:i])
+            d2 = dataset_from_numpy(d2_feats[:i], d2_targets[:i])
+            dist = DatasetDistance(d1, d2, inner_ot_method="exact")
+            d = dist.distance(maxsamples=1000)
+            distances.append(d.item())
+            completed_timestamps.append(d1_timestamps[i - 1])
+        except Exception as e:
+            print(f"Skipping {i} due to {e}...")
+            continue
+
+    return completed_timestamps, distances
--- datacollect/utils/post_processing/extract.py
+++ datacollect/utils/post_processing/extract.py
@@ -1,15 +1,64 @@
-def extract_dataset(env):
-    """Returns a dictionary of point labels collected by each agent.
+import pickle
+
+import numpy as np
+
+
+def collections(env):
+    """Returns a dict of point labels collected by each agent with timestamps.
 
     Args:
         env (AECEnv): Env to extract point labels from.
 
     Returns:
-        dict: Dictionary of point labels keyed by agent name.
+        dict: Dict of point labels together with timestamps keyed by agent
+            name.
     """
-    point_labels_collected = {}
+    colls = {}
     for agent in env.possible_agents:
         collector = env.collectors[agent]
-        point_labels = [point.label for point in collector.points]
-        point_labels_collected[agent] = point_labels
-    return point_labels_collected
+        points_and_timestamps = [
+            [point.label, timestamp] for point, timestamp in collector.points
+        ]
+        colls[agent] = points_and_timestamps
+    return colls
+
+
+def save_collections(env, filename):
+    """Extracts and saves point labels collected by each agent with timestamps.
+
+    Args:
+        env (AECEnv): Env to extract point labels from.
+        filename (str): Filename to save point labels to.
+    """
+    colls = collections(env)
+    with open(filename, "wb") as f:
+        pickle.dump(colls, f)
+        print(f"Saved collections to {filename}.")
+
+
+def feats_targets_timestamps(collections, include_timestamps=True):
+    """Extracts features, targets and timestamps from collections.
+
+    Args:
+        collections (dict): Dict of point labels together with timestamps keyed
+            by agent name.
+        include_timestamps (bool, optional): Whether to include timestamps in
+            features. Defaults to True.
+
+    Returns:
+        tuple: Tuple of timestamps, features and targets.
+    """
+    timestamps = []
+    feats = []
+    targets = []
+    for i, feat in enumerate(collections.values()):
+        timestamps += [f[1] for f in feat]
+        if not include_timestamps:
+            feat = [[f[0]] for f in feat]
+        feats += feat
+        targets += [i] * len(feat)
+    sort_indices = np.argsort(timestamps)
+    feats = np.array(feats)[sort_indices]
+    targets = np.array(targets)[sort_indices]
+    timestamps = np.array(timestamps)[sort_indices]
+    return timestamps, feats, targets
